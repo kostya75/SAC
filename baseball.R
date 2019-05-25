6ee97f5a124b66fe613c5e47a3a43538353f385e
kosta <k_mingou@hotmail.com>
2019-05-24 20:17:11 -0400

    may 24

--- baseball.R
+++ baseball.R
@@ -24,7 +24,34 @@ plotpattern<-function(df){
   ggplot(df,aes(x=cyear,y=rbi/ab))+geom_line()+xlim(xlim)+ylim(ylim)
 }
 
+pdf("paths.pdf", width = 8, height = 4)
+d_ply(baseball, .(reorder(id, rbi / ab)), failwith(NA, plotpattern), .print = TRUE)
+dev.off()
+
+
+
+
 
 pdf("paths.pdf", width = 8, height = 4)
 d_ply(baseball, .(reorder(id, rbi / ab)), failwith(NA, plotpattern),.print = TRUE)
 dev.off()
+
+
+bmodel<-function(df) lm(rbi/ab~cyear,data=df)
+
+bmodels<-dlply(baseball,.(id),bmodel)
+
+tt<-bmodel(baberuth)
+rsq<-function(x) summary(x)$r.squared
+bcoefs<-ldply(bmodels,function(x) c(coef(x),rsquare=rsq(x)))
+names(bcoefs)[2:3]<-c("intercept","slope")
+
+baseballcoef<-merge(baseball,bcoefs,by="id")
+subset(baseballcoef,rsquare>=0.99)$id
+
+ggplot(bcoefs, aes(slope, intercept)) + 
+  geom_point(aes(size = rsquare), alpha = 0.5) +
+  geom_vline(xintercept = 0, size=0.5, colour="grey50") + 
+  geom_hline(yintercept = 0, size = 0.5, colour="grey50")
+last_plot()+xlim(c(-.01,.01))
+
